d17b7128f21aeb7e7a8198e896286c0c7dc2ab38
Daniel Thalmann <daniel@thalmann.li>
2023-10-28 16:25:14 +0200

    add hit capability

--- src/shape/Sphere.cpp
+++ src/shape/Sphere.cpp
@@ -6,15 +6,33 @@ Hit Sphere::hit(Ray r)
 {
     Vector3 oc = r.origin() - center;
     double a = dot(r.direction(), r.direction());
-    double b = 2.0 * dot(oc, r.direction());
+    double half_b = dot(oc, r.direction());
     double c = dot(oc, oc) - radius*radius;
-    double discriminant = b*b - 4*a*c;
+    double discriminant = half_b * half_b - a*c;
     //return (discriminant >= 0);
     Hit hit;
+
+    hit.t = -1;
+
+    double ray_tmin = 0.1;
+    double ray_tmax = 4.0;
+
+    double sqrtd = sqrt(discriminant);
+    // Find the nearest root that lies in the acceptable range.
+    double root = (-half_b - sqrtd) / a;
+    if (root <= ray_tmin || ray_tmax <= root) {
+        root = (-half_b + sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root)
+            return hit;
+    }
+
+
     if (discriminant < 0) {
         hit.t = -1.0;
     } else {
-        hit.t = (-b - sqrt(discriminant) ) / (2.0*a);
+        hit.t = root;
+        hit.point = r.at(hit.t);
+        hit.normal = (hit.point - center) / radius;
     }
     return hit;
 
